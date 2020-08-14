85221fefe08a2cb6990b9b18226be7bf4d945235
Zach <zrb@umd.edu>
2020-08-13 21:08:22 -0400

    first draft of notebook

--- models/chlorophyll_autograd.py
+++ models/chlorophyll_autograd.py
@@ -1,74 +1,123 @@
-# -*- coding: utf-8 -*-
-# https://pytorch.org/tutorials/beginner/pytorch_with_examples.html#pytorch-tensors-and-autograd
+# https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
+
+from datetime import datetime, timedelta
+import math
+import os
+
+from matplotlib import pyplot
 import numpy
 import pandas
 import torch
 
+from utilities.utilities import get_logger
+
+LOGGER = get_logger('chlorophyll.autograd', os.path.join(__file__, os.pardir, os.pardir, 'logs/chlorophyll_autograd.log'))
+
+TRAINING_CSV_FILENAME = '../data/training.csv'
+VALIDATION_CSV_FILENAME = '../data/validation.csv'
+INPUT_TESTING_CSV_FILENAME = '../data/testing.csv'
+OUTPUT_TESTING_CSV_FILENAME = '../data/testing_output_autograd.csv'
+
 if __name__ == '__main__':
-    assert torch.cuda.is_available()
+    # take advantage of cuda device if it exists
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    dtype = torch.float
-    device = torch.device('cuda')
+    # define data type for tensors
+    tensor_data_type = torch.float
 
-    training_data = pandas.read_csv('../data/training.csv')
-    testing_data = pandas.read_csv('../data/testing.csv')
-    validation_data = pandas.read_csv('../data/validation.csv')
+    # load data from files
+    training_data_frame = pandas.read_csv(TRAINING_CSV_FILENAME)
+    validation_data_frame = pandas.read_csv(VALIDATION_CSV_FILENAME)
+    testing_data_frame = pandas.read_csv(INPUT_TESTING_CSV_FILENAME)
+    columns = training_data_frame.columns
 
-    # input size
+    # covert read data to numpy arrays
+    training_data = training_data_frame.to_numpy()
+    validation_data = validation_data_frame.to_numpy()
+    testing_data = testing_data_frame.to_numpy()
+
+    # size of input per-site (all wavelengths)
     D_in = 16
-    # hidden size
-    H = 100
-    # output size
+    # size of output per-site (just chlorophyll)
     D_out = 1
+    # size of hidden layer
+    H = math.ceil(D_in * 2 / 3 + D_out)
 
-    # load training data
-    training_array = training_data.to_numpy()
-    reflectance = training_array[:, :-1]
-    chlorophyll = numpy.expand_dims(training_array[:, -1], axis=1)
+    # training input / output tensors
+    training_reflectance_tensor = torch.tensor(training_data[:, :-1], device=device, dtype=tensor_data_type)
+    training_chlorophyll_tensor = torch.tensor(numpy.expand_dims(training_data[:, -1], axis=1), device=device, dtype=tensor_data_type)
 
-    x = torch.tensor(reflectance, device=device, dtype=dtype)
-    y = torch.tensor(chlorophyll, device=device, dtype=dtype)
+    # validation input tensor and output data
+    validation_reflectance_tensor = torch.tensor(validation_data[:, :-1], device=device, dtype=tensor_data_type)
+    validation_chlorophyll_data = numpy.expand_dims(validation_data[:, -1], axis=1)
 
-    # Create random Tensors for weights.
-    # Setting requires_grad=True indicates that we want to compute gradients with
-    # respect to these Tensors during the backward pass.
-    w1 = numpy.array(())
-    w2 = numpy.array(())
-    w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
-    w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+    # testing input tensor
+    testing_reflectance_tensor = torch.tensor(testing_data, device=device, dtype=tensor_data_type)
 
-    learning_rate = 1e-6
-    for t in range(500):
-        # Forward pass: compute predicted y using operations on Tensors; these
-        # are exactly the same operations we used to compute the forward pass using
-        # Tensors, but we do not need to keep references to intermediate values since
-        # we are not implementing the backward pass by hand.
-        y_pred = x.mm(w1).clamp(min=0).mm(w2)
+    # generate random weight tensors
+    weight_1 = torch.randn(D_in, H, device=device, dtype=tensor_data_type, requires_grad=True)
+    weight_2 = torch.randn(H, D_out, device=device, dtype=tensor_data_type, requires_grad=True)
 
-        # Compute and print loss using operations on Tensors.
-        # Now loss is a Tensor of shape (1,)
-        # loss.item() gets the scalar value held in the loss.
-        loss = (y_pred - y).pow(2).sum()
-        if t % 100 == 99:
-            print(t, loss.item())
+    start_time = datetime.now()
 
-        # Use autograd to compute the backward pass. This call will compute the
-        # gradient of loss with respect to all Tensors with requires_grad=True.
-        # After this call w1.grad and w2.grad will be Tensors holding the gradient
-        # of the loss with respect to w1 and w2 respectively.
+    # hold times and losses here for plotting later
+    times = [datetime.now()]
+    losses = [0]
+
+    # iterate over specified passes at defined learning rate
+    learning_rate = 1e-8
+    passes = 10000
+
+    LOGGER.info(f'starting {passes} optimization passes on autograd model ({D_in}->{H}->{D_out}) w/ {learning_rate} learning rate')
+
+    for pass_index in range(passes):
+        # calculate predicted output manually
+        predicted_chlorophyll_tensor = training_reflectance_tensor.mm(weight_1).clamp(min=0).mm(weight_2)
+
+        # calculate loss as sum of squared errors
+        loss = (predicted_chlorophyll_tensor - training_chlorophyll_tensor).pow(2).sum()
+
+        # collect data point every 100 iterations
+        if pass_index % 100 == 99:
+            losses.append(loss.item())
+            times.append(datetime.now())
+            LOGGER.info(f'{pass_index:>6} - {losses[-1]:>12} ({losses[-1] - losses[-2]:>12} difference)')
+
+        # add gradients to weights `.grad`
         loss.backward()
 
-        # Manually update weights using gradient descent. Wrap in torch.no_grad()
-        # because weights have requires_grad=True, but we don't need to track this
-        # in autograd.
-        # An alternative way is to operate on weight.data and weight.grad.data.
-        # Recall that tensor.data gives a tensor that shares the storage with
-        # tensor, but doesn't track history.
-        # You can also use torch.optim.SGD to achieve this.
+        # apply gradients to weights (don't know how this works but there it is)
         with torch.no_grad():
-            w1 -= learning_rate * w1.grad
-            w2 -= learning_rate * w2.grad
+            weight_1 -= learning_rate * weight_1.grad
+            weight_2 -= learning_rate * weight_2.grad
 
-            # Manually zero the gradients after updating weights
-            w1.grad.zero_()
-            w2.grad.zero_()
+            # clear gradients from weights so they are applied fresh next iteration
+            weight_1.grad.zero_()
+            weight_2.grad.zero_()
+
+    LOGGER.info(f'optimization took {(datetime.now() - start_time) / timedelta(seconds=1):.2} seconds')
+
+    # plot loss and loss change rate over time
+    figure = pyplot.figure()
+    figure.suptitle(f'autograd loss w/ {passes} passes @ {learning_rate} learning rate')
+    axis = figure.add_subplot(1, 1, 1)
+    axis.plot(times, losses, label='loss')
+    axis.plot(times, numpy.concatenate(([0], numpy.diff(losses))), label='loss dx')
+    axis.legend()
+    pyplot.show()
+
+    # compare predicted output with validation input
+    validation_chlorophyll_tensor = validation_reflectance_tensor.mm(weight_1).clamp(min=0).mm(weight_2)
+    validation_predicted_chlorophyll = validation_chlorophyll_tensor.detach().cpu().numpy()
+    validation_differences = validation_chlorophyll_data - validation_predicted_chlorophyll
+    validation_rmse = numpy.sqrt(numpy.mean(numpy.square(validation_differences)))
+    LOGGER.info(f'RMSE of validation chlorophyll vs predicted chlorophyll: {validation_rmse:.3}')
+    LOGGER.info(f'standard deviation of validation chlorophyll:            {numpy.std(validation_chlorophyll_data):.3}')
+
+    # predict output of testing dataset
+    testing_chlorophyll_tensor = testing_reflectance_tensor.mm(weight_1).clamp(min=0).mm(weight_2)
+    testing_predicted_chlorophyll = testing_chlorophyll_tensor.detach().cpu().numpy()
+    testing_data_frame.insert(len(testing_data_frame.columns), 'Chl', testing_predicted_chlorophyll, True)
+    testing_data_frame.to_csv(OUTPUT_TESTING_CSV_FILENAME)
+
+    print('done')
--- models/chlorophyll_nn.py
+++ models/chlorophyll_nn.py
@@ -1,20 +1,28 @@
 # https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
+
 from datetime import datetime, timedelta
 import math
+import os
 
 from matplotlib import pyplot
 import numpy
 import pandas
 import torch
 
+from utilities.utilities import get_logger
+
+LOGGER = get_logger('chlorophyll.nn', os.path.join(__file__, os.pardir, os.pardir, 'logs/chlorophyll_nn.log'))
+
 TRAINING_CSV_FILENAME = '../data/training.csv'
 VALIDATION_CSV_FILENAME = '../data/validation.csv'
 INPUT_TESTING_CSV_FILENAME = '../data/testing.csv'
-OUTPUT_TESTING_CSV_FILENAME = '../data/testing_output.csv'
+OUTPUT_TESTING_CSV_FILENAME = '../data/testing_output_nn.csv'
 
 if __name__ == '__main__':
+    # take advantage of cuda device if it exists
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # define data type for tensors
     tensor_data_type = torch.float
 
     # load data from files
@@ -23,6 +31,7 @@ if __name__ == '__main__':
     testing_data_frame = pandas.read_csv(INPUT_TESTING_CSV_FILENAME)
     columns = training_data_frame.columns
 
+    # covert read data to numpy arrays
     training_data = training_data_frame.to_numpy()
     validation_data = validation_data_frame.to_numpy()
     testing_data = testing_data_frame.to_numpy()
@@ -34,65 +43,85 @@ if __name__ == '__main__':
     # size of hidden layer
     H = math.ceil(D_in * 2 / 3 + D_out)
 
+    # training input / output tensors
     training_reflectance_tensor = torch.tensor(training_data[:, :-1], device=device, dtype=tensor_data_type)
     training_chlorophyll_tensor = torch.tensor(numpy.expand_dims(training_data[:, -1], axis=1), device=device, dtype=tensor_data_type)
+
+    # validation input tensor and output data
     validation_reflectance_tensor = torch.tensor(validation_data[:, :-1], device=device, dtype=tensor_data_type)
     validation_chlorophyll_data = numpy.expand_dims(validation_data[:, -1], axis=1)
 
+    # testing input tensor
     testing_reflectance_tensor = torch.tensor(testing_data, device=device, dtype=tensor_data_type)
 
+    # build three-layer model, from input size to hidden layer to output size
     model = torch.nn.Sequential(
         torch.nn.Linear(D_in, H),
         torch.nn.ReLU(),
         torch.nn.Linear(H, D_out)
     )
+    # ensure model is on CUDA device if available
     if torch.cuda.is_available():
         model.cuda()
 
     # define loss function as sum of mean square error
     loss_function = torch.nn.MSELoss(reduction='sum')
 
-    learning_rate = 0.01
-
-    # use Adam algorithm with liberal learning rate to start
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
     start_time = datetime.now()
 
+    # hold times and losses here for plotting later
     times = [datetime.now()]
     losses = [0]
-    for optimize_index in range(20000):
-        predicted_chlorophyll = model(training_reflectance_tensor)
-        mse_loss = loss_function(predicted_chlorophyll, training_chlorophyll_tensor)
-        if optimize_index % 100 == 0:
+
+    # use Adam algorithm with a liberal learning rate to start
+    learning_rate = 0.01
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # optimize model over a specified number of iterations
+    passes = 10000
+
+    LOGGER.info(f'starting {passes} optimization passes on NN model ({D_in}->{H}->{D_out}) w/ {learning_rate} learning rate')
+
+    for pass_index in range(passes):
+        # predict output using current state of model, and calculate loss
+        predicted_chlorophyll_tensor = model(training_reflectance_tensor)
+        mse_loss = loss_function(predicted_chlorophyll_tensor, training_chlorophyll_tensor)
+
+        # collect data point every 100 iterations
+        if pass_index % 100 == 99:
             losses.append(mse_loss.item())
             times.append(datetime.now())
-            print(f'{optimize_index:>6} - {losses[-1]:>12} ({losses[-1] - losses[-2]:>12} difference) - learning rates: {[param_group["lr"] for param_group in optimizer.param_groups]}')
+            LOGGER.info(f'{pass_index:>6} - {losses[-1]:>12} ({losses[-1] - losses[-2]:>12} difference) - learning rates: {[param_group["lr"] for param_group in optimizer.param_groups]}')
+
+        # recalculate gradients of current state
         optimizer.zero_grad()
         mse_loss.backward()
+
+        # iterate to the next step based on current learning rate
         optimizer.step()
 
-    print(f'optimization took {(start_time - datetime.now()) / timedelta(seconds=1)} seconds')
+    LOGGER.info(f'optimization took {(datetime.now() - start_time) / timedelta(seconds=1):.4} seconds')
 
+    # plot loss and loss change rate over time
     figure = pyplot.figure()
-    figure.suptitle('loss')
+    figure.suptitle(f'NN loss w/ {passes} passes @ {learning_rate} learning rate')
     axis = figure.add_subplot(1, 1, 1)
     axis.plot(times, losses, label='loss')
     axis.plot(times, numpy.concatenate(([0], numpy.diff(losses))), label='loss dx')
     axis.legend()
-
     pyplot.show()
 
+    # compare predicted output with validation input
     validation_chlorophyll_tensor = model(validation_reflectance_tensor)
     validation_predicted_chlorophyll = validation_chlorophyll_tensor.detach().cpu().numpy()
     validation_differences = validation_chlorophyll_data - validation_predicted_chlorophyll
     validation_rmse = numpy.sqrt(numpy.mean(numpy.square(validation_differences)))
-    print(f'RMSE of validation chlorophyll vs predicted chlorophyll: {validation_rmse}\n'
-          f'standard deviation of validation chlorophyll:            {numpy.std(validation_chlorophyll_data)}')
+    LOGGER.info(f'RMSE of validation chlorophyll vs predicted chlorophyll: {validation_rmse}')
+    LOGGER.info(f'standard deviation of validation chlorophyll:            {numpy.std(validation_chlorophyll_data)}')
 
+    # predict output of testing dataset
     testing_chlorophyll_tensor = model(testing_reflectance_tensor)
     testing_predicted_chlorophyll = testing_chlorophyll_tensor.detach().cpu().numpy()
-
     testing_data_frame.insert(len(testing_data_frame.columns), 'Chl', testing_predicted_chlorophyll, True)
     testing_data_frame.to_csv(OUTPUT_TESTING_CSV_FILENAME)
 
