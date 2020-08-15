ef277f89c2c8aa35877a05bbf21ba6fea4f57552
Zach <zrb@umd.edu>
2020-08-15 00:16:52 -0400

    setup hidden layer size perturbation

---
+++ models/3_nn_optimH.py
@@ -0,0 +1,150 @@
+"""
+Optimize PyTorch neural net model to sample set, plot loss over time, log RMSE, and write final predictions.
+
+This script uses a fixed size for the hidden layer, defined as 2/3 the input layer plus output layer.
+"""
+
+from datetime import datetime, timedelta
+import math
+import os
+
+from matplotlib import pyplot
+import numpy
+import pandas
+import torch
+
+from models.utilities.utilities import get_logger
+
+LOGGER = get_logger('chlorophyll.nn', os.path.join(__file__, os.pardir, 'logs/3_nn_optimH.log'))
+
+TRAINING_CSV_FILENAME = '../data/training.csv'
+VALIDATION_CSV_FILENAME = '../data/validation.csv'
+INPUT_TESTING_CSV_FILENAME = '../outputs/0_input/testing.csv'
+OUTPUT_TESTING_CSV_FILENAME = '../outputs/3_nn_optimH/testing.csv'
+
+if __name__ == '__main__':
+    # take advantage of cuda device if it exists
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # define data type for tensors
+    tensor_data_type = torch.float
+
+    # load data from files
+    training_data_frame = pandas.read_csv(TRAINING_CSV_FILENAME)
+    validation_data_frame = pandas.read_csv(VALIDATION_CSV_FILENAME)
+    testing_data_frame = pandas.read_csv(INPUT_TESTING_CSV_FILENAME)
+    columns = training_data_frame.columns
+
+    # covert read data to numpy arrays
+    training_data = training_data_frame.to_numpy()
+    validation_data = validation_data_frame.to_numpy()
+    testing_data = testing_data_frame.to_numpy()
+
+    # size of input per-site (all wavelengths)
+    input_layer_size = 16
+    # size of output per-site (just chlorophyll)
+    output_layer_size = 1
+    # size of hidden layer
+    default_hidden_layer_size = math.ceil(input_layer_size * 2 / 3 + output_layer_size)
+
+    # training input / output tensors
+    training_reflectance_tensor = torch.tensor(training_data[:, :-1], device=device, dtype=tensor_data_type)
+    training_chlorophyll_tensor = torch.tensor(numpy.expand_dims(training_data[:, -1], axis=1), device=device, dtype=tensor_data_type)
+
+    # validation input tensor and output data
+    validation_reflectance_tensor = torch.tensor(validation_data[:, :-1], device=device, dtype=tensor_data_type)
+    validation_chlorophyll_data = numpy.expand_dims(validation_data[:, -1], axis=1)
+
+    # testing input tensor
+    testing_reflectance_tensor = torch.tensor(testing_data, device=device, dtype=tensor_data_type)
+
+    # define loss function as sum of mean square error
+    loss_function = torch.nn.MSELoss(reduction='sum')
+
+    start_time = datetime.now()
+
+    # hold times and RMSEs here for plotting later
+    times = [datetime.now()]
+    rmses = [0]
+    lowest_rmse = numpy.Inf
+    lowest_rmse_hidden_layer_size = None
+    lowest_rmse_model = None
+
+    # optimize model over a specified number of iterations
+    passes = 10000
+
+    # iterate over a range of hidden layer sizes
+    for hidden_layer_size in range(default_hidden_layer_size, input_layer_size * 5):
+
+        # build three-layer model, from input size to hidden layer to output size
+        model = torch.nn.Sequential(
+            torch.nn.Linear(input_layer_size, hidden_layer_size),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_layer_size, output_layer_size)
+        )
+        # ensure model is on CUDA device if available
+        if torch.cuda.is_available():
+            model.cuda()
+
+        # use Adam algorithm with a liberal learning rate to start
+        learning_rate = 0.001
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+        LOGGER.info(f'starting {passes} optimization passes on NN model ({input_layer_size}->{hidden_layer_size}->{output_layer_size}) w/ {learning_rate} learning rate')
+
+        for pass_index in range(passes):
+            # predict output using current state of model, and calculate loss
+            predicted_chlorophyll_tensor = model(training_reflectance_tensor)
+            mse_loss = loss_function(predicted_chlorophyll_tensor, training_chlorophyll_tensor)
+
+            # recalculate gradients of current state
+            optimizer.zero_grad()
+            mse_loss.backward()
+
+            # iterate to the next step based on current learning rate
+            optimizer.step()
+
+        # compare predicted output with validation input
+        validation_chlorophyll_tensor = model(validation_reflectance_tensor)
+        validation_predicted_chlorophyll = validation_chlorophyll_tensor.detach().cpu().numpy()
+        validation_differences = validation_chlorophyll_data - validation_predicted_chlorophyll
+        validation_rmse = numpy.sqrt(numpy.mean(numpy.square(validation_differences)))
+
+        # collect data point every 100 iterations
+        times.append(datetime.now())
+        rmses.append(validation_rmse)
+        LOGGER.info(f'optimization took {(datetime.now() - start_time) / timedelta(seconds=1):.4} seconds')
+        LOGGER.info(f'RMSE: {validation_rmse:.3}; hidden layer size: {hidden_layer_size}')
+
+        if validation_rmse < lowest_rmse:
+            lowest_rmse = validation_rmse
+            lowest_rmse_hidden_layer_size = hidden_layer_size
+            lowest_rmse_model = model
+
+        LOGGER.info(f'lowest RMSE: {lowest_rmse:.3}; hidden layer size of lowest RMSE: {lowest_rmse_hidden_layer_size}')
+
+    LOGGER.info(f'standard deviation of validation chlorophyll:            {numpy.std(validation_chlorophyll_data)}')
+    LOGGER.info(f'model with lowest RMSE ({lowest_rmse}) had a hidden layer size of {lowest_rmse_hidden_layer_size}')
+
+    # predict output of testing dataset
+    testing_chlorophyll_tensor = lowest_rmse_model(testing_reflectance_tensor)
+    testing_predicted_chlorophyll = testing_chlorophyll_tensor.detach().cpu().numpy()
+    testing_data_frame.insert(len(testing_data_frame.columns), 'Chl', testing_predicted_chlorophyll, True)
+    testing_data_frame.to_csv(OUTPUT_TESTING_CSV_FILENAME)
+
+    # remove first dummy entries from times and losses
+    times = times[1:]
+    rmses = rmses[1:]
+
+    # plot loss and loss change rate over time
+    figure = pyplot.figure()
+    figure.suptitle(f'NN RMSE vs size of (single) hidden layer w/ {passes} passes')
+    axis = figure.add_subplot(1, 1, 1)
+    axis.plot(times, rmses, label='RMSE')
+    axis.plot(times, numpy.concatenate(([0], numpy.diff(rmses))), label='RMSE dx')
+    axis.axhline(y=0, color='k', linestyle='--')
+    axis.legend()
+    pyplot.show()
+    pyplot.savefig('../notebooks/images/3_nn_optimH.png')
+
+    print('done')
--- models/utilities/plot_training_data.py
+++ models/utilities/plot_training_data.py
@@ -7,23 +7,23 @@ import numpy
 import pandas
 
 if __name__ == '__main__':
-    training_data = pandas.read_csv('../../data/training.csv')
-    training_data = training_data.sort_values('Chl')
+    training_data_frame = pandas.read_csv('../../data/training.csv')
+    training_data_frame = training_data_frame.sort_values('Chl')
 
-    wavelengths = list(training_data.columns)[:-1]
+    wavelengths = list(training_data_frame.columns)[:-1]
     wavelengths[0] = wavelengths[0][2:]
     wavelengths = [float(wavelength) for wavelength in wavelengths]
 
     color_map = pyplot.cm.viridis
-    color_normalizer = colors.Normalize(vmin=0, vmax=len(training_data))
+    color_normalizer = colors.Normalize(vmin=0, vmax=len(training_data_frame))
 
-    chlorophyll_mininmum = training_data['Chl'].min()
-    chlorophyll_maxinmum = training_data['Chl'].max()
+    chlorophyll_mininmum = training_data_frame['Chl'].min()
+    chlorophyll_maxinmum = training_data_frame['Chl'].max()
 
     figure = pyplot.figure()
     axis = figure.add_subplot(1, 1, 1, projection='3d')
 
-    for site_index, site in training_data.iloc[::100, :].iterrows():
+    for site_index, site in training_data_frame.iloc[::100, :].iterrows():
         axis.plot(xs=numpy.full([len(wavelengths)], fill_value=chlorophyll_mininmum + ((chlorophyll_maxinmum - chlorophyll_mininmum) * color_normalizer(site_index))),
                   ys=wavelengths,
                   zs=site[:-1],
